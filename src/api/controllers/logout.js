74d24b73b087210d892f4b0af44c142a39086aa5
YolandaRm <yramosmartin@gmail.com>
2020-06-11 12:38:11 +0200

    logout

--- src/api/controllers/logout.js
+++ src/api/controllers/logout.js
@@ -1,5 +1,13 @@
 module.exports = (api) => {
     api.post('/logout', (req, res) => {
         // TODO: eliminar la cookie de refresco
+        const refreshCookie = req.$.config.authenticaction.refreshCookie
+        res
+            .clearCookie(refreshCookie, { 
+                httpOnly: true,
+                sameSite:'none'
+            })
+            .status(204)
+            .end();
     });
 }
