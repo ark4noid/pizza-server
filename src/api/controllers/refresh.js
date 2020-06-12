b510603a775997df8b260ceb1c5c15dec7b096d4
Isidro Torregrosa <isistorre95@gmail.com>
2020-06-12 11:46:03 +0200

    register y refresh

--- src/api/controllers/refresh.js
+++ src/api/controllers/refresh.js
@@ -1,6 +1,37 @@
 module.exports = (api) => {
     api.post('/refresh', (req, res) => {
+        const { jwt, config } = req.$;
+        const {
+            refreshCookie,
+            refreshSecret,
+            authSecret,
+            algorithm,
+            authTTL
+        } = config.authentication;
+        const refreshToken = req.cookies[refreshCookie];
+
+        // si no hay token
+        if (!refreshToken) {
+            res.status(401).end();
+            return;
+        }
+
         // TODO: comprobar que el token de refresco sea valido
+        let payload;
+        try {
+            payload = await jwt.verify(refreshToken, refreshSecret);
+        } catch (err) {
+            // si el token es invalido
+            res.status(401).end();
+            return;
+        }
+        const user = payload.user;
         // TODO: emitir un nuevo token de auth
+        const authToken = await jwt.sign({ user }, authSecret, {
+            algorithm,
+            expiresIn: authTTL
+        });
+
+        res.json({token: authToken});
     });
 }
--- src/api/controllers/register.js
+++ src/api/controllers/register.js
@@ -1,7 +1,36 @@
+const bcrypt = require('bcrypt');
 module.exports = (api) => {
-    api.post('/register', (req, res, next) => {
-        // TODO: modificar la contrasena del body usando bcrypt
-        // TODO: utilizar req.$.dbManager para grabar el usuario
-        // req.$.dbManager.create('users', body)
+    api.post('/register', async (req, res, next) => {
+        const { dbManager } = req.$;
+        const { email, password, name } = req.body;
+        // comprobar que no exista usuario con el email
+        const userExists = await dbManager.get('users', {
+            find: {
+                email
+            }
+        });
+
+        if(userExists){
+            res.status(400).json({
+                errors: {
+                    email: 'An user already exists with that email'
+                }
+            });
+            return;
+        }
+        // encriptar password
+        const encryptedPassword = await bcrypt.hash(password, 10); // 10 rondas del algoritmo
+
+        // crear usuario
+        const user = await dbManager.create('users', {
+            name,
+            email,
+            password: encryptedPassword
+        });
+
+        res.status(201).json({
+            ...user,
+            password: undefined
+        });
     })
 }
